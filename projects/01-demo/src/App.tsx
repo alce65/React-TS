977fb0e4f9d71f4519e04928b2378eaf1f269063
Alejandro Cerezo <alce65@hotmail.es>
2025-05-23 11:30:49 +0200

    Add and rebuild demo-01 initial vite project with react and TS

---
+++ projects/01-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Initial } from "./components/initial";
+
+function App() {
+  return (
+    <>
+      <h1>Demo 01</h1>
+      <Initial />
+    </>
+  );
+}
+
+export default App;
