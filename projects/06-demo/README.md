a6dab0f95b0f6b3e9ca4a1fe6856ca16e4c6f1d1
Alejandro Cerezo <alce65@hotmail.es>
2025-05-24 10:46:16 +0200

    Add and rebuild demo-06 initial Vite project with react, TS and Vitest. Include a tested component Header

---
+++ projects/06-demo/README.md
@@ -0,0 +1,13 @@
+# 06-Demo: React + TypeScript + Vite + Vitest
+
+Proyecto con React, TypeScript y Vite.
+Reubicado y modificado el ejemplo inicial.
+Dependencias añadidas: vitest, jsdom y testing-library/react.
+
+## Initial project
+
+Por ser parte de un monorepo, de la instalación inicial de Vite con React y TypeScript se elimina
+
+- .gitignore
+- eslint.config.js
+- el contenido original del readme
---
+++ projects/06-demo/src/components/header/header.tsx
@@ -0,0 +1,16 @@
+import reactLogo from '../../assets/react.svg';
+import './header.css';
+
+type Props = {
+    title: string;
+};
+export const Header: React.FC<Props> = ({ title = 'Demo 06' }) => {
+    return (
+        <header className="header">
+            <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+            <h1>{title}</h1>
+        </header>
+    );
+};
